b4630f466af1ae1f198f95ae93035d272359eeca
ro cant sleep <115865263+icebrrrr@users.noreply.github.com>
2022-11-26 00:33:02 -0800

    draft hoodie sizing
    "hoodie" sizing py

---
+++ test/bottoms.py
@@ -0,0 +1,6 @@
+else;
+    waist = int(input("waist size: "))
+    inseam = int(input("inseam length "))
+    Thigh-width = int(input("Thigh width "))
+    leg-opening = int(input("Leg opening width"))
+    rise = int(input("rise width"))
---
+++ test/test.py
@@ -0,0 +1,28 @@
+tops = str(input("what kind of top?"))
+# hoodies, sweaters and shirts etc 
+if tops == "hoodie";
+    print("to messure ")
+    # fast fashion products on average shring 4% after washing 
+    if brand == "fast fashion";
+        washes = int(input("have you washed this garmen? "))
+        if washes == "yes"
+            score
+
+
+
+    chest = int(input("chest width: "))
+    length = int(input("length: "))
+    s-length = int(input("sleave length: "))
+    bicep = int(input("bicep width"))
+    hem = int(input("hem width"))
+    shoulder = int(input("shoulder "))
+
+    brand = str(input("area of fashion? (fast fashion, vintage or high)"))
+    
+
+
+    
+    
+    
+    
+    fit = str(input("oversized, tight or tts"))
